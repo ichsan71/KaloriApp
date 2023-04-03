20f039ac4b3aae0691b2c60f2e71a2d09333fa36
Muhammad Ichsan Kamil <58933009+ichsan71@users.noreply.github.com>
2023-04-04 05:41:21 +0700

    Tambah UI history dan statistic

--- app/src/main/java/com/marqumil/kaloriapp/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/marqumil/kaloriapp/ui/history/HistoryViewModel.kt
@@ -1,10 +1,10 @@
-package com.marqumil.kaloriapp.ui.dashboard
+package com.marqumil.kaloriapp.ui.history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class HistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
---
+++ app/src/main/java/com/marqumil/kaloriapp/ui/notifications/StatisticFragment.kt
@@ -0,0 +1,50 @@
+package com.marqumil.kaloriapp.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.marqumil.kaloriapp.databinding.FragmentStatisticBinding
+
+class StatisticFragment : Fragment() {
+
+    private var _binding: FragmentStatisticBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val statisticViewModel =
+            ViewModelProvider(this)[StatisticViewModel::class.java]
+
+        _binding = FragmentStatisticBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textNotifications
+//        statisticViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.toolbarBack?.setOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
